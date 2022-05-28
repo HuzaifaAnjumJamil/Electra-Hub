6195b63c72432deda373ad27db834cbffbaed27c
Disha D <disha16122001@gmail.com>
2022-05-29 00:44:23 +0530

    Updated files

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -7,7 +7,13 @@ import { useStateContext } from '../../context/StateContext';
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
     const [index, setIndex] = useState(0);
-    const { decQty, incQty, qty, onAdd} = useStateContext();
+    const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
+
+    const handleBuyNow = () => {
+        onAdd(product, qty);
+
+        setShowCart(true);
+    }
 
   return (
     <div>
@@ -20,6 +26,7 @@ const ProductDetails = ({ product, products }) => {
                 className="small-images-container">
                     {image?.map((item, i) => (
                     <img 
+                    key={i}
                     src={urlFor(item)} className={i === index ? 'small-image selected-image' :'small-image'} onMouseEnter={() => setIndex(i)} 
                     />
                 ))}
@@ -45,13 +52,13 @@ const ProductDetails = ({ product, products }) => {
                 <h3>Quantity:</h3>
                 <p className="quantity-desc">
                     <span className="minus" onClick={decQty}><AiOutlineMinus /></span>
-                    <span className="num" onClick="">{qty}</span>
+                    <span className="num">{qty}</span>
                     <span className="plus" onClick={incQty}><AiOutlinePlus /></span>
                 </p>
             </div>
             <div className="buttons">
                 <button type="button" className="add-to-cart" onClick={() => onAdd(product, qty)}>Add to Cart</button>
-                <button type="button" className="buy-now" onClick="">Buy Now</button>
+                <button type="button" className="buy-now" onClick={handleBuyNow}>Buy Now</button>
             </div>
             </div>          
         </div>
