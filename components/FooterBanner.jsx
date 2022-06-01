8c3f013580a71e7eb6ee2a43c15dd4648508caf3
Disha D <disha16122001@gmail.com>
2022-06-01 19:56:51 +0530

    Updated FIles

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -16,7 +16,6 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
 
         <div className="right">
           <p>{smallText}</p>
-          <h3>{midText}</h3>
           <p>{desc}</p>
           <Link href={`/product/${product}`}>
             <button type="button">{buttonText}</button>
--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ const Home = ({ products, bannerData }) => (
       <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
       <div className="products-heading">
-        <h2>Best Selling Products</h2>
-        <p>Speakers of many variations</p>
+        <h2>The Gadget Store</h2>
+        <p>BEST SELLING PRODUCTS</p>
       </div>
 
       <div className="products-container">
